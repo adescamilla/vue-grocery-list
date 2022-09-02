ac234e44b65b2e79fcc7c504ba1cf5831e9a7854
Andrew Escamilla <aescami6@stu.southtexascollege.edu>
2022-09-01 21:38:35 -0500

    Added bg color/styling

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,16 @@
 </script>
 
 <template>
-<h1 class="title">📝 Vue Grocery List 📝</h1>
+  <main>
+    <h1 class="title">📝 Vue Grocery List 📝</h1>
+  </main>
 </template>
 
 <style scoped>
+  main {
+    @apply flex flex-col justify-center items-center;
+  }
   .title {
-    @apply m-2 text-6xl font-light tracking-wider;
+    @apply m-2 text-6xl font-light tracking-wider text-accent;
   }
 </style>
